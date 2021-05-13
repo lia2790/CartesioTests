8460b03013322cf081968c47d09fc213ce268955
lia2790 <liana.bertoni@iit.it>
2021-05-13 18:41:06 +0200

    CartesioTestsCpp-fixed bugs

--- CartesioTestsCpp/src/CartesioTestsHandler.cpp
+++ CartesioTestsCpp/src/CartesioTestsHandler.cpp
@@ -69,7 +69,7 @@ void CartesioTestsHandler::setTaskTargetTime(double target_time)
     // set reference
     Eigen::Affine3d Tref;
     _task->getPoseReference(Tref);
-    Tref.translation()[0] += 5.0;
+    Tref.translation()[1] += 5.0;
     setTaskTarget(Tref, target_time);
 }
 
