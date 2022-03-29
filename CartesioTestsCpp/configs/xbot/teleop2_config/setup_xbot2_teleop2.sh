4c8d60bf7fc99283392b8b07b0eb9c4f65642bba
lia2790 <liana.bertoni@gmail.com>
2022-03-29 11:03:49 +0200

    Revert "CartesioTestsCpp-added teleoperation robot 2"
    This reverts commit 55a35d0619edd99eb0ee486a13af0b0f99f14db3.

--- CartesioTestsCpp/configs/xbot/teleop2_config/setup_xbot2_teleop2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set_xbot2_config /home/liana/catkin_ws/src/CartesioTests/CartesioTestsCpp/configs/xbot/teleop2_config/teleop2_basic.yaml
-export  XBOT_ROOT=/home/liana/catkin_ws/src/CartesioTests/CartesioTestsCpp
-xbot2-core --verbose --hw dummy
